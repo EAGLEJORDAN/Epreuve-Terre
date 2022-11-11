800b8b97261ab4f5dd10c0cd5800699b89029fc9
EAGLEJORDAN <114489625+EAGLEJORDAN@users.noreply.github.com>
2022-11-11 19:04:23 +0100

    Delete terre06.js

--- terre06.js
+++
@@ -1,9 +0,0 @@
-function reverserstring (parametre1)
-{
-	var resultat1=parametre1.split('').reverse().join("");
-	console.log(resultat1);
-}
-
-var salutation;
-salutation="lehciM";
-reverserstring(salutation);
