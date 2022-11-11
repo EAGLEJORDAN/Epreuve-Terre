606417d667b3bb39b62f6d11357e903fffe90af6
EAGLEJORDAN <114489625+EAGLEJORDAN@users.noreply.github.com>
2022-11-11 19:04:13 +0100

    Delete terre05.js

--- terre05.js
+++
@@ -1,8 +0,0 @@
-function diviser (parametre1, parametre2)
-{ 
-	resultat=parametre1/parametre2
-	reste=parametre1%parametre2
-console.log("resultat: "+resultat);
-return("reste: "+reste);
-}
-diviser (40,2);
