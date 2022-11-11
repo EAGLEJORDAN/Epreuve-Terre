1663abacc9b1e601f6a12f8332a8ad778bcc8e80
EAGLEJORDAN <114489625+EAGLEJORDAN@users.noreply.github.com>
2022-11-11 19:03:06 +0100

    Delete terre02.js

--- terre02.js
+++
@@ -1,5 +0,0 @@
-function mot_dacceuil (parametre1, parametre2, parametre3, parametre4)
-{
-return("Bienvenue "+"\n"+parametre1+ "\n"+parametre2+ "\n"+parametre3+ "\n"+parametre4);
-}
-mot_dacceuil("je", "suis", "solide", "!");
